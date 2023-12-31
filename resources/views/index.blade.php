ece1a7e2010bcff5f8ffab0e8b7806cce3127e43
Jay Millena <jay.millena07@gmail.com>
2023-12-31 13:41:28 +0800

    Practicing seeder, factory, migration, model, controller, route, blade templating, direvatives

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+@section('section')
+<div class="main">
+  <h1>Main</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis tempore, culpa accusantium non nisi consectetur assumenda eos? Omnis voluptas quos, consequuntur repudiandae non excepturi ipsa enim officiis laudantium architecto iure.</p>
+</div>
+@endsection
