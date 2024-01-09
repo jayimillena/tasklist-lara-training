3b8f406121e2aede72ac51e0f6ff491e2a42be18
Jay Millena <jay.millena07@gmail.com>
2024-01-09 12:41:33 +0800

    Session flash messeges add for update tasks

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -9,14 +9,23 @@
     <div>
       <label for="title">Title</label>
       <input type="text" name="title" id="title" value="{{ $task->title }}" /> 
+      @error('title')
+        <p>{{ $message }}</p>
+      @enderror
     </div>
     <div>
       <label for="description">Description</label>
       <textarea name="description" id="description" cols="30" rows="10">{{ $task->description }}</textarea>
+      @error('description')
+        <p>{{ $message }}</p>
+      @enderror
     </div>
     <div>
       <label for="long_description">Long Description</label>
       <textarea name="long_description" id="long_description" cols="30" rows="10">{{ $task->long_description }}</textarea>
+      @error('long_description')
+        <p>{{ $message }}</p>
+      @enderror
     </div>
     <div>
       <button type="submit">Update Task</button>
